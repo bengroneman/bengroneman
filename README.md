e9db1d517cf8c8f46d490f80b1dd860588f87523
bengroneman <bluegroneman10@gmail.com>
2022-07-30 19:26:16 -0600

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Ben Groneman
+
 A mission driven engineer with a drive to solve user experience challenges with data-driven and intuitive solutions. I bring diverse experience in Web Development, data engineering, consulting, with a soft spot for JavaScript.
--- src/routes/gallery/index.svelte
+++ src/routes/gallery/index.svelte
@@ -8,15 +8,15 @@
 </script>
 
 <ul class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-	{#if images }
+	{#if images}
 		{#each images as image}
 			<li class="relative">
 				<div
 					class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
 				>
 					<img
-						src="{image}"
-						alt="{image}"
+						src={image}
+						alt={image}
 						class="object-cover pointer-events-none group-hover:opacity-75"
 					/>
 				</div>
